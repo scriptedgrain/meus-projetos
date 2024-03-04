d00a9e617dd01bc6547de5044020e4bba8770724
Lucca Maia <luccamairosa@gmail.com>
2024-03-04 17:31:37 -0300

    novo teste

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Boas vindas turma DIO");
+        System.out.println("Oi amigos!");
     }
 }
